c45f8f5ebd56b6f7fd8ff5b743a9f1fb6f5e26e3
Valentin Mayer-Eichberger <vale1410@gmail.com>
2021-10-02 18:45:45 +0200

    more instances

--- make_benchmark.sh
+++ make_benchmark.sh
@@ -7,10 +7,12 @@ mkdir -p $output_folder
 
 go build encode.go
 
+#for enc in {143,16}
 for enc in {143,16}
 do 
     #for x in {gttt-3x3,gttt-4x4}
-    for x in {gttt-3x3,gttt-4x4,gttt-5x5-iterative-deepening,hex-hein,milestones}
+    #for x in {gttt-3x3,gttt-4x4,gttt-5x5-iterative-deepening,hex-hein,milestones}
+    for x in {gttt-3x3,hex-hein}
     do 
             mkdir -p $output_folder/$enc 
             mkdir -p $output_folder/$enc/$x 
